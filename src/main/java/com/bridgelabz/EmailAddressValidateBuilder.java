75a316ee63ead62c27700fd9381ca933d3114ede
anupama cv <anu.anupamacv@gmail.com>
2021-05-26 00:46:40 +0530

    ADDED: pattern to validate 2 nd part of email id

--- src/main/java/com/bridgelabz/EmailAddressValidateBuilder.java
+++ src/main/java/com/bridgelabz/EmailAddressValidateBuilder.java
@@ -16,7 +16,7 @@ public class EmailAddressValidateBuilder
     public static void main(String[] args)
     {
         String input = "anu123.";
-        String patternNum = "^[a-zA-Z0-9-._]{3,}$";
+        String patternNum = "^[a-zA-Z0-9_-.]{3,}$";
         outputChecker(checkPattern(input, patternNum));
     }
     private static void outputChecker(boolean checkPattern)
