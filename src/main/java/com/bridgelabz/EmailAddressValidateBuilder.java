83ad686116334d6194c3641fa64cf0faf0f9b2a1
anupama cv <anu.anupamacv@gmail.com>
2021-05-26 00:41:44 +0530

    ADDED: pattern to validate 1 st part of email id

--- src/main/java/com/bridgelabz/EmailAddressValidateBuilder.java
+++ src/main/java/com/bridgelabz/EmailAddressValidateBuilder.java
@@ -1,10 +1,33 @@
 package com.bridgelabz;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class EmailAddressValidateBuilder
 {
+    public static boolean checkPattern(String input, String patternNum)
+    {
+        Pattern pattern = Pattern.compile(patternNum);
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find())
+        {
+            return true;
+        }
+        return false;
+    }
     public static void main(String[] args)
     {
-        System.out.println("Welcome");
+        String input = "anu123.";
+        String patternNum = "^[a-zA-Z0-9-._]{3,}$";
+        outputChecker(checkPattern(input, patternNum));
     }
-
+    private static void outputChecker(boolean checkPattern)
+    {
+        if(checkPattern==true)
+        {
+            System.out.println("Valid");
+        }
+        else
+        {
+            System.out.println("InValid");
+        }
+    }
 }
